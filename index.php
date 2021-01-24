4df5da32f064197a5944e4ff6da67db930f02462
unknown <tuan8190571@gmail.com>
2021-01-24 16:04:36 +0700

    tao cai moi

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bai 20</title>
+	</head>
+	<body>
+		<p>This is 7<sup>th</sup> Program Using HTML</p>	
+		<br>
+		<p> H2O Is The Chemical Name For Water</p>
+		<br>
+		<pre>
+*   *********   *********
+*           *           *
+*           *           *
+*   *********   *********
+*   *                   *
+*   *                   *
+*   *********   *********
+		
+		</pre>
+			
+		<del> This Text Will Apprear With Strike Effect</del>	
+	</body>
+</html> 
